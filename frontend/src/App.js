3368e41496ec2cbb605f77c8d5e4b52e05a862cc
Enoc Aguilar <enoap10@google.com>
2024-02-08 17:50:00 -0600

    create react, node js and html

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [userName, setUsername] = useState('')
+  useEffect(() =>{
+    getNames();
+  }, [])
+  const getNames = async () => {
+    const response = await axios.get('/names');
+    console.log(response);
+  }
+  return (
+    <div >
+      <h1>Hello Enoc</h1>
+    </div>
+  );
+}
+
+export default App;
